f9c47e1fc35c31b684d83190f7f2cf0fe0022238
khanh <letrongkhanh26112004@gmail.com>
2025-12-10 15:55:08 +0700

    khanh

---
+++ Assets/Scripts/SetNavigationTarget.cs
@@ -0,0 +1,40 @@
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class SetNavigationTarget : MonoBehaviour
+{
+    [SerializeField]
+    private Camera topDownCamera;
+    [SerializeField]
+    private GameObject navTargetObject;
+
+    private NavMeshPath path; // current calculated path
+    private LineRenderer line; // linerenderer to display path
+
+    private bool lineToggle = false;
+
+    // Unity Message | 0 references
+    private void Start()
+    {
+        path = new NavMeshPath();
+        line = transform.GetComponent<LineRenderer>();
+    }
+
+    // Unity Message | 0 references
+    private void Update()
+    {
+        if ((Input.touchCount > 0) && (Input.GetTouch(0).phase == TouchPhase.Began))
+        {
+            lineToggle = !lineToggle;
+        }
+
+        if (lineToggle)
+        {
+            NavMesh.CalculatePath(transform.position, navTargetObject.transform.position, NavMesh.AllAreas, path);
+            line.positionCount = path.corners.Length;
+            line.SetPositions(path.corners);
+            line.enabled = true;
+        }
+    }
+}
